9b505e800906b6c2c03b2cf382dc80671bda1334
yDewolf <andrersng@gmail.com>
2025-03-25 14:08:58 -0300

    Sudoku é muito difícil

--- Exercicios2/Sudoku/Grid.java
+++ Exercicios2/Sudoku/Grid.java
@@ -1,16 +1,20 @@
 package Exercicios2.Sudoku;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Grid {
     public int size_x;
     public int size_y;
 
     public int[][] values;
 
-    public Grid(int size_x, int size_y) {
+    public Grid(int size_x, int size_y, int max_fill) {
         this.size_x = size_x;
         this.size_y = size_y;
 
         this.values = new int[size_y][size_x];
+        fill_randomly_rows(max_fill);
     }
 
     public void insert_value(int row_idx, int column_idx, int value) {
@@ -21,40 +25,101 @@ public class Grid {
         this.values[row_idx][column_idx] = 0;
     }
 
-    // Check if the row sums up to the correct value
-    public boolean check_row(int row_idx) {
-        int sum = 0;        
+    // Checks if a row is valid
+    public boolean check_row(int row_idx, int values[][], boolean check_win) {
+        int[] numbers = new int[this.size_x];
         for (int i = 0; i < this.size_x; i++) {
             // Check if there is position without a value
+            if (this.values[row_idx][i] == 0 && check_win) {
+                return false;
+            } 
+
+            if (Arrays.asList(numbers).contains(values[row_idx][i])) {
+                return false;
+            }
+
             if (this.values[row_idx][i] == 0) {
-                return false;
-            } 
-            sum += this.values[row_idx][i];
+                continue;
+            }
+            
+            numbers[i] = this.values[row_idx][i];
         }
 
-        return sum == sum_to(this.size_x);
+        return true;
     }
 
-    // Check if the column sums up to the correct value
-    public boolean check_column(int column_idx) {
-        int sum = 0;        
-        for (int i = 0; i < this.size_x; i++) {
+    // Checks if a column is valid
+    public boolean check_column(int column_idx, int values[][], boolean check_win) {
+        int[] numbers = new int[this.size_y];
+        for (int i = 0; i < this.size_y; i++) {
             // Check if there is position without a value
-            if (this.values[i][column_idx] == 0) {
+            if (this.values[i][column_idx] == 0 && check_win) {
                 return false;
             } 
-            sum += this.values[i][column_idx];
+
+            if (Arrays.asList(numbers).contains(values[i][column_idx])) {
+                return false;
+            }
+            
+            if (this.values[i][column_idx] == 0) {
+                continue;
+            }
+
+            numbers[i] = this.values[i][column_idx];
         }
 
-        return sum == sum_to(this.size_x);
+        return true;
     }
 
-    private int sum_to(int max) {
-        int sum = 0;
-        for (int i = 1; i <= max; i++) {
-            sum += i;
+    public boolean check_position_correct(int x_pos, int y_pos, int value) {
+        int[][] check_values = new int[this.size_y][this.size_x];
+        for (int y = 0; y < this.size_y; y++) {
+            for (int x = 0; x < this.size_x; x++) {
+                check_values[x][y] = this.values[x][y];
+            }
         }
 
-        return sum;
+        
+        check_values[x_pos][y_pos] = value;
+
+        return check_row(y_pos, check_values, false) && check_column(x_pos, check_values, false);
+    }
+
+    private void fill_randomly_rows(int max) {
+        Random rng = new Random();
+        for (int row_idx = 0; row_idx < this.size_y; row_idx++) {
+            int[] already_in = new int[this.size_x];
+
+            for (int i = 0; i < max; i++) {
+                int num = 0;
+                do  {
+                    num = rng.nextInt(1, this.size_x); // Randi range from 0 to normally 9
+                }
+                while (Arrays.asList(already_in).contains(num));
+
+                int column_idx = 0;
+                do {
+                    column_idx = rng.nextInt(1, this.size_x);
+                } 
+                while (!check_position_correct(column_idx, row_idx, num));
+
+                insert_value(row_idx, column_idx, num);
+                already_in[column_idx] = num;
+            }
+        } 
+    }
+
+
+    public String stringify() {
+        String stringified = "";
+        for (int y = 0; y < this.size_y; y++) {
+            for (int x = 0; x < this.size_x; x++) {
+                stringified += " " + this.values[x][y] + " ";
+            }
+
+            stringified += "\n";
+        }
+
+        return stringified;
     }
 }
--- Exercicios2/Sudoku/Sudoku.java
+++ Exercicios2/Sudoku/Sudoku.java
@@ -1,40 +1,14 @@
 package Exercicios2.Sudoku;
+import Exercicios2.Sudoku.Grid;
 
 import java.util.Random;
 
 public class Sudoku {
     public static void main(String[] args) {
-        int[] grid = create_grid(false, 3);
-        System.out.println(stringify_grid(grid, 3, 3));
+        Grid grid = new Grid(3, 3, 1);
+        System.out.println(grid.stringify());
+
+        // int[] grid = create_grid(false, 3);
+        // System.out.println(stringify_grid(grid, 3, 3));
     }  
-
-    private static void put_random_row(int[] grid, int row_idx) {
-        
-    }
-
-    private static int[] create_grid(boolean random, int amount) {
-        Random rng = new Random();
-        int[] single_grid = new int[9];    
-
-        for (int i = 0; i < amount; i++) {
-            int num = rng.nextInt(0, 9);
-            int idx = rng.nextInt(0, 9);
-            single_grid[idx] = num;
-        }
-
-        return single_grid;
-    }
-
-    private static String stringify_grid(int[] grid, int y_size, int x_size) {
-        String stringified = "";
-        for (int y = 0; y < y_size; y++) {
-            for (int x = 0; x < x_size; x++) {
-                stringified += " " + grid[(y * 3) + x] + " ";
-            }
-
-            stringified += "\n";
-        }
-
-        return stringified;
-    }
 }
